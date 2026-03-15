46414bdb22ad1284b233fad4da3ef31dca4012e0
glenn-47 <g47doyle@gmail.com>
2026-03-15 21:11:58 +0000

    created new main class & added console testing prior to GUI launch

---
+++ DocklandsBioTracker/src/biotracker/Main.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package biotracker;
+
+/**
+ *
+ * @author glenn
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("=============================================");
+        System.out.println("   STARTING CONSOLE TEST   ");
+        System.out.println("=============================================\n");
+
+        // Initialise the Manager
+        DataManager testManager = new DataManager();
+        System.out.println("[TEST] DataManager initialised with pre-loaded data.");
+        System.out.println("Initial Registry Size: " + testManager.getAllRecords().size());
+
+        // Test the Queue (Add & Peek)
+        System.out.println("\n--- TESTING QUEUE (ENQUEUE & PEEK) ---");
+        EcoRecord testFlora = new FloraRecord("TEST-001", "Trinity College", "2026-03-15", "Rose", "Blooming");
+        testManager.addRecord(testFlora);
+        System.out.println("Added 'TEST-001' to Pending Queue.");
+        System.out.println("Peek at Queue Front: " + testManager.peekNextPending().displayDetails());
+
+        // Test Queue Dequeue (Approve Next)
+        System.out.println("\n--- TESTING QUEUE (DEQUEUE TO LINKED LIST) ---");
+        EcoRecord approved = testManager.approveNextPending();
+        System.out.println("Approved Record: " + approved.getRecordId());
+        System.out.println("Registry Size is now: " + testManager.getAllRecords().size());
+
+        // Test Singly Linked List (Delete) & Stack (Push)
+        System.out.println("\n--- TESTING SINGLY LINKED LIST (REMOVE) & STACK (PUSH) ---");
+        boolean isDeleted = testManager.deleteRecord("TEST-001");
+        System.out.println("Successfully deleted TEST-001? " + isDeleted);
+        System.out.println("Registry Size after deletion: " + testManager.getAllRecords().size());
+
+        // Test Stack (Pop / Undo)
+        System.out.println("\n--- TESTING STACK (POP / UNDO) ---");
+        EcoRecord restored = testManager.undoLastDeletion();
+        System.out.println("Restored Record: " + restored.getRecordId());
+        System.out.println("Final Registry Size: " + testManager.getAllRecords().size());
+
+        System.out.println("\n=============================================");
+        System.out.println("   CONSOLE TESTING COMPLETE. LAUNCHING GUI... ");
+        System.out.println("=============================================\n");
+
+        // Launch the GUI
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainGUI().setVisible(true);
+            }
+        });
+    }
+    
+}
--- DocklandsBioTracker/src/biotracker/MainGUI.java
+++ DocklandsBioTracker/src/biotracker/MainGUI.java
@@ -319,7 +319,7 @@ public class MainGUI extends javax.swing.JFrame {
             javax.swing.JOptionPane.showMessageDialog(this, "Undo successful! Record " + restored.getRecordId() + " has been restored to the main registry.");
             refreshDisplay();
         } else {
-            javax.swing.JOptionPane.showMessageDialog(this, "Nothing to restore.", "Stack Empty", javax.swing.JOptionPane.INFORMATION_MESSAGE);
+            javax.swing.JOptionPane.showMessageDialog(this, "Nothing to restore.", "Error", javax.swing.JOptionPane.INFORMATION_MESSAGE);
         }
     }//GEN-LAST:event_btnUndoActionPerformed
 
@@ -331,27 +331,7 @@ public class MainGUI extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
-            logger.log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new MainGUI().setVisible(true));
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnApproveNext;
